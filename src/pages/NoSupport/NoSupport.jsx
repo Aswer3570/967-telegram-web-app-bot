2c60a0d1229bf2f706af49acbfc011a7df0983d4
Aswer3570 <fedreg2012@yandex.ru>
2024-01-20 02:45:53 +0400

    Add

--- src/pages/NoSupport/NoSupport.jsx
+++ src/pages/NoSupport/NoSupport.jsx
@@ -1,10 +1,26 @@
 import './NoSupport.scss'
+import constructionIcon from '../../assets/construction.png'
+import QRCodeIcon from "../../assets/qrcode.svg?react"
 
 function NoSupport() {
 	return (
-		<>
-			<p>Error</p>
-		</>
+		<div className="no-support__container">
+			<div className="no-support__wrapper">
+				<img src={constructionIcon} className="construction-icon" alt="Construction" />
+
+				<p className="no-support__title">
+					Desktop not supported
+				</p>
+
+				<p className="no-support__description">
+					Use a mobile device
+				</p>
+
+				<div className="qr-code__container">
+					<QRCodeIcon className="qr-code__icon" />
+				</div>
+			</div>
+		</div>
 	)
 }
 
