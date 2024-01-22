185a260601968c392f5b45374765c71dc765bc6d
Aswer3570 <fedreg2012@yandex.ru>
2024-01-22 18:26:01 +0400

    Fix

--- src/components/BottomSlider/BottomSlider.jsx
+++ src/components/BottomSlider/BottomSlider.jsx
@@ -1,7 +1,14 @@
 import './BottomSlider.scss'
 import CloseIcon from "../../assets/close.svg?react"
+import { useWebApp } from '@vkruglikov/react-telegram-web-app'
 
 function BottomSlider({img, title, description, buttonTitle}) {
+	const webApp = useWebApp()
+
+	function test() {
+		webApp.openTelegramLink('/wallet?startattach=wpay_order-orderId__8838532657153')
+	}
+
 	return (
 		<div className="bottom-slider__container">
 			<div className="bottom-slider__close-container">
@@ -20,7 +27,7 @@ function BottomSlider({img, title, description, buttonTitle}) {
 				{ description }
 			</p>
 
-			<button className="bottom-slider__button">
+			<button className="bottom-slider__button" onClick={test()}>
 				{ buttonTitle }
 			</button>
 		</div>
