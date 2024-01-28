432d261dc2ba15b71cc2bc2091aa41b3d57e1f6e
Aswer3570 <fedreg2012@yandex.ru>
2024-01-28 14:35:26 +0400

    All fix

--- src/InitializationProvider.jsx
+++ src/InitializationProvider.jsx
@@ -28,36 +28,3 @@ const DeviceRedirect = ({ children }) => {
 }
 
 export default DeviceRedirect
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useEffect } from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import { useWebApp } from '@vkruglikov/react-telegram-web-app'
-
-// const DeviceRedirect = ({ children }) => {
-// 	const navigate = useNavigate()
-// 	const webApp = useWebApp()
-
-// 	useEffect(() => {
-// 		// unknown и macos - удалить потом
-// 		if (webApp.platform !== 'ios' && webApp.platform !== 'android' && webApp.platform !== 'macos' && webApp.platform !== 'unknown') {
-// 			return navigate('/no-support')
-// 		}
-// 	}, [])
-
-// 	return <>{ children }</>
-// }
-
-// export default DeviceRedirect
